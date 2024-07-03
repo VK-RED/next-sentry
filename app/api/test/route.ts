159ea989cd6513020920aad658e592347165282c
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-03 12:29:34 +0530

    modify test endPoint

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,8 +1,10 @@
 import * as Sentry from "@sentry/nextjs";
 
 
+let user = 0;
+
 export async function GET(req:Request){
-    Sentry.metrics.set("user_view", "jane");
+    Sentry.metrics.set("user_view", `${++user}`,{tags:{"doneBy":`${user}`}});
     console.log("Hit the Test endPoint");
-    return Response.json({message:"Returing a sample message from the test endpoint !!"});
+    return Response.json({message:"Returing a sample message from the test endpoint with the user set to :"+user});
 }
