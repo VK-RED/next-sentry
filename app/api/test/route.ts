c06a5ec1b8efecc10b2b58947f5e74575ad4a4d9
Vishnu Kumar <vishnukumart96@gmail.com>
2024-07-03 12:46:20 +0530

    add increment metric on testendpoint

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -5,6 +5,7 @@ let user = 0;
 
 export async function GET(req:Request){
     Sentry.metrics.set("user_view", `${++user}`,{tags:{"doneBy":`${user}`}});
+    Sentry.metrics.increment("any_test_endpoint",1,{tags:{setBy:"author"}});
     console.log("Hit the Test endPoint");
     return Response.json({message:"Returing a sample message from the test endpoint with the user set to :"+user});
 }
